dcf2d9de4d3b3179f7a2cc103e55f27e50481fc7
danielcs1993 <danielcs1993@hotmail.com>
2018-04-02 16:49:00 -0300

    Estado listando

--- abas/lista-estado.php
+++ abas/lista-estado.php
@@ -1,3 +1,28 @@
 <?php 
 $estados = enviarRequisicao('estado/listar', null)->body;
 ?>
+
+<div class="container">
+   		
+   		<div class="page-header">
+   			<h1>Estados:</h1>
+   		</div>
+
+   		<table class="table table-striped table-bordered table-hover table-condensed">
+   			<thead>
+   				<tr>
+   					<th>Sigla</th>
+   					<th>Nome</th>
+   				</tr>
+   			</thead>
+   			<tbody>
+   			<?php foreach($estados as $estado) { ?>	
+   				<tr >
+   					<td><?php echo $estado->uf; ?></td>
+   					<td><?php echo $estado->nomeEstado; ?></td>
+   				</tr>
+   			<?php } ?>
+   			</tbody>
+   		</table>
+
+   	</div> 
