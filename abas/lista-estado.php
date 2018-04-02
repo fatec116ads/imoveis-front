e2029e86b76ab7b79aaafddcf52fb0cbeb865d3c
danielcs1993 <danielcs1993@hotmail.com>
2018-04-02 16:57:41 -0300

    Tela crua completa listando via rest

--- abas/lista-estado.php
+++ abas/lista-estado.php
@@ -4,15 +4,20 @@ $estados = enviarRequisicao('estado/listar', null)->body;
 
 <div class="container">
    		
+   		<div class="col-md-3" style="float:right; margin-top: 50px">
+			<button class="btn btn-success" style="width: 100%">+ Inserir novo Estado</button>
+		</div>
    		<div class="page-header">
    			<h1>Estados:</h1>
    		</div>
+   		
 
    		<table class="table table-striped table-bordered table-hover table-condensed">
    			<thead>
    				<tr>
    					<th>Sigla</th>
    					<th>Nome</th>
+   					<th></th>
    				</tr>
    			</thead>
    			<tbody>
@@ -20,6 +25,17 @@ $estados = enviarRequisicao('estado/listar', null)->body;
    				<tr >
    					<td><?php echo $estado->uf; ?></td>
    					<td><?php echo $estado->nomeEstado; ?></td>
+   					<td>
+   						<a href="https://www.google.com.br/">
+        				<span class="glyphicon glyphicon-pencil"></span>
+      					</a>
+      					<a href="https://www.google.com.br/">
+        				<span class="glyphicon glyphicon-search"></span>
+      					</a>
+      					<a href="https://www.google.com.br/">
+        				<span class="glyphicon glyphicon-remove"></span>
+      					</a>
+      				</td>
    				</tr>
    			<?php } ?>
    			</tbody>
